9007e5307d5ae08d90a6a25ae304d7ce6b93f9ea
rem <61095277+rem-my@users.noreply.github.com>
2021-05-02 18:40:54 +0100

    no dmca today

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* ModuleFinder, a Vizality plugin to ease plugin development
- * Copyright (C) 2021 rem
+ * Copyright (C) 2021 Vendicated
  *
  * ModuleFinder is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
